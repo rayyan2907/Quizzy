e4f52a99c8d9347818188545e03209c0395cf8e1
Muhammad Rayyan <mrayyan5296@gmail.com>
2025-04-18 10:58:04 +0500

    Creating Project Files

---
+++ Quizzy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quizzy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
