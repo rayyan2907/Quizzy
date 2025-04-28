b3cb974520c88cca921ff4ac12262deeb073becb
dev-codee <faizanahd289@gmail.com>
2025-04-28 20:49:40 +0500

    added make_quiz view and altered mcqs table in db

---
+++ quizzy project files/Models/Buisness_Models/Quiz_models.cs
@@ -0,0 +1,20 @@
+﻿namespace Quizzy.Models.Buisness_Models
+{
+    public class Quiz_models
+    {
+        public int mcq_id { get; set; }
+        public string description { get; set; }
+        public string opt1 { get; set; }
+        public string opt2 { get; set; }
+        public string? opt3 { get; set; }
+        public string? opt4 { get; set; }
+        public string corr_opt { get; set; }
+        public string? img_path { get; set;}
+    }
+    public class short_model
+    {
+        public string question { get; set; }
+        public string? image_path { get; set; }
+    }
+
+}
