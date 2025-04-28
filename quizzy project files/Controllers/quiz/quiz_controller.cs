745fbd37c3ce1f3273920549d5d78dddbdedcdb2
Muhammad Rayyan <mrayyan5296@gmail.com>
2025-04-28 22:01:10 +0500

    some minor changes

---
+++ quizzy project files/Controllers/quiz/quiz_controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quizzy.Controllers.quiz
+{
+    public class quizController : Controller
+    {
+        public IActionResult attemptQuiz()
+        {
+            return View("make_quiz");
+        }
+    }
+}
