b34e859e7fe1eeb844c2d665b44ef04240373543
up33anas <anassabir1072@gmail.com>
2025-05-02 21:07:09 +0500

    Implemented delete, update, assign and unassign quiz functionality

--- quizzy project files/Models/Buisness_Layer/quiz/createQuizBL.cs
+++ quizzy project files/Models/Buisness_Layer/quiz/createQuizBL.cs
@@ -1,6 +1,22 @@
-﻿namespace Quizzy.Models.Buisness_Layer.quiz
+﻿using Quizzy.Models.Data_Layer.quiz;
+
+namespace Quizzy.Models.Buisness_Layer.quiz
 {
     public class createQuizBL
     {
+        public static bool deleteQuiz(string id)
+        {
+            return createQuizDL.deleteQuiz(id);
+        }
+
+        public static bool assignQuiz(string id)
+        {
+            return createQuizDL.assignQuiz(id);
+        }
+
+        public static bool unassignQuiz(string id)
+        {
+            return createQuizDL.assignQuiz(id);
+        }
     }
 }
--- quizzy project files/Models/Data_Layer/quiz/createQuizDL.cs
+++ quizzy project files/Models/Data_Layer/quiz/createQuizDL.cs
@@ -1,6 +1,57 @@
-﻿namespace Quizzy.Models.Data_Layer.quiz
+﻿using DBHelper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Quizzy.Models.Data_Layer.quiz
 {
     public class createQuizDL
     {
+        public static bool deleteQuiz(string id)
+        {
+            try
+            {
+                string query = $"DELETE FROM quiz WHERE quizID = {id}";
+
+                int rowsAffected = DatabaseHelper.Instance.Update(query);
+                return rowsAffected > 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in delete Quiz: " + ex.Message);
+                return false;
+            }
+        }
+
+        public static bool assignQuiz(string id)
+        {
+            try
+            {
+                string query = $"UPDATE quiz SET is_asssigned = true WHERE quizID = {id}";
+
+                int rowsAffected = DatabaseHelper.Instance.Update(query);
+                return rowsAffected > 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in assigning Quiz: " + ex.Message);
+                return false;
+            }
+        }
+
+        public static bool unassignQuiz(string id)
+        {
+            try
+            {
+                string query = $"UPDATE quiz SET is_asssigned = false WHERE quizID = {id}";
+
+                int rowsAffected = DatabaseHelper.Instance.Update(query);
+                return rowsAffected > 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in unassigning Quiz: " + ex.Message);
+                return false;
+            }
+        }
     }
-}
+}        
