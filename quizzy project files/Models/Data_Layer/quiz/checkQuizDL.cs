2772c56ca4be76f442f884041bef90133e3ce666
up33anas <anassabir1072@gmail.com>
2025-05-08 23:48:57 +0500

    Scehma Updates and Check quiz

--- quizzy project files/Models/Data_Layer/quiz/checkQuizDL.cs
+++ quizzy project files/Models/Data_Layer/quiz/checkQuizDL.cs
@@ -16,21 +16,28 @@ namespace Quizzy.Models.Data_Layer.quiz
         {
             string query = $@"
                 SELECT 
-                    CONCAT(s.addmission_year, '-', s.dept, '-', s.roll_num) AS registration_number, 
-                    CONCAT(s.first_name, ' ', s.last_name) AS name, 
-                    COUNT(CASE WHEN m.correct_opt = a.answer THEN 1 END) AS mcqs_marks,
-                    IFNULL(SUM(sc.marks), 0) AS sqs_marks
+                    CONCAT(s.addmission_year, '-', s.dept, '-', s.roll_num) AS registration_number,
+                    CONCAT(s.first_name, ' ', s.last_name) AS name,
+                    COUNT(CASE 
+                             WHEN m.correct_opt = ma.answer 
+                             THEN 1 
+                         END) AS mcqs_marks,
+                    IFNULL(SUM(sc.marks), 0) AS sqs_marks,
+                    COUNT(CASE 
+                             WHEN m.correct_opt = ma.answer 
+                             THEN 1 
+                         END) + IFNULL(SUM(sc.marks), 0) AS total_marks
                 FROM attempt apt
-                INNER JOIN students s ON s.studentID = apt.studentID
-                LEFT JOIN mcq_answers a ON a.studentID = s.studentID
-                LEFT JOIN mcqs m ON m.mcqID = a.mcqID AND m.quizID = apt.quizID
+                JOIN students s ON s.studentID = apt.studentID
+                LEFT JOIN mcq_answers ma ON ma.studentID = s.studentID
+                LEFT JOIN mcqs m ON m.mcqID = ma.mcqID AND m.quizID = apt.quizID
                 LEFT JOIN shq_check sc ON sc.studentID = s.studentID
                 LEFT JOIN short_questions sq ON sq.shqID = sc.shqID AND sq.quizID = apt.quizID
-                WHERE apt.quizID = 9
-                GROUP BY s.studentID, s.addmission_year, s.dept, s.roll_num, s.first_name, s.last_name;"; 
+                WHERE apt.quizID = 101
+                GROUP BY 
+                    s.studentID, s.addmission_year, s.dept, s.roll_num, s.first_name, s.last_name;";
 
             return DatabaseHelper.Instance.GetData(query);
         }
-
     }
 }
